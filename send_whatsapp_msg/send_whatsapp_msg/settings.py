9bf282604d52d87ff39818963824702bc7e86e3c
sagar.nimbustech <sagar.nimbustech@gmail.com>
2022-03-29 17:48:17 +0530

    secret key disbaled

--- send_whatsapp_msg/send_whatsapp_msg/settings.py
+++ send_whatsapp_msg/send_whatsapp_msg/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2!mq_j7v5c3=ghyeuh-r%oa1(v$)_0*%527zl#yee&c*#vhn9m'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
